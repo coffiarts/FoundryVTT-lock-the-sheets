c1a686a17b10fb0a5e5daa1f2bcf1f55af50e05c
coffiarts <coffiarts@github.com>
2025-09-03 13:51:17 +0200

    v13 compatibility (fixing config menu issue)

--- src/lock-the-sheets/scripts/logger.js
+++ src/lock-the-sheets/scripts/logger.js
@@ -6,9 +6,18 @@ export class Logger {
         // Register game settings relevant to this class specifically (all globally relevant settings are maintained by class Config)
 
         // create separator and title at the beginning of this settings section
-        Hooks.on('renderSettingsConfig', (app,[html]) => {
-            html.querySelector(`[data-setting-id="${Config.data.modID}.debug"]`).insertAdjacentHTML('beforeBegin', `<h3>Logging</h3>`)
-        })
+        if (Config.getGameMajorVersion() >= 13) {
+            Hooks.on('renderSettingsConfig', (app, html) => {
+                const inputEl = html.querySelector(`#settings-config-${Config.data.modID.replace(/\./g, "\\.")}\\.debug`);
+                const formGroup = inputEl.closest(".form-group");
+                formGroup.insertAdjacentHTML("beforebegin", `<h4 style="margin-top: 0; border-bottom: 1px solid #888; padding-bottom: 4px; margin-bottom: 6px;">Logging</h4>`);
+            });
+        }
+        else {
+            Hooks.on('renderSettingsConfig', (app, [html]) => {
+                html.querySelector(`[data-setting-id="${Config.data.modID}.debug"]`).insertAdjacentHTML('beforeBegin', `<h3>Logging</h3>`)
+            });
+        }
 
         const settingsData = {
             debug : {
